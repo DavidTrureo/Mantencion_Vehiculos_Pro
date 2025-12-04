665cd15a50522b7de65eed000d4b8105f8dabc52
David Trureo Ahumada <david.trureo.ahumada@hotmail.com>
2025-12-03 22:14:12 -0300

    feat(data): Implementa Room para la base de datos de usuarios

--- MantenimientoVehiculosPro/app/build.gradle.kts
+++ MantenimientoVehiculosPro/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -61,11 +63,20 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    // --- Hilt ---
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
     // --- Data & Networking ---
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
     implementation(libs.okhttp.logging)
     implementation(libs.androidx.datastore.preferences)
+
+    // --- Room ---
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
 
     // --- QR Generation ---
     implementation(libs.zxing.core)
